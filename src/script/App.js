b7dac3ee8754502397dbe03a117673858f3fafa1
Maxime Tamburrini <maximemickael.contact@gmail.com>
2022-10-12 16:08:29 +0200

    update link to json

--- src/script/App.js
+++ src/script/App.js
@@ -20,7 +20,7 @@ export class App {
     }
     handleDataFromJson() {
         return __awaiter(this, void 0, void 0, function* () {
-            const api = new Api("recipes.json");
+            const api = new Api("/recipes.json");
             this._fetchedData = yield api.fetchData();
             this._allReceipts = this._fetchedData.map(data => new Recette(data));
             return this._allReceipts;
